d9a57456c289169c52f86af5da6ea2052ad1444f
Timur Aliev <timaaliev35@gmail.com>
2022-06-20 12:06:31 +0300

    first com

---
+++ src/main/java/app/servlets/DateServlet.java
@@ -0,0 +1,35 @@
+package app.servlets;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        resp.setContentType("text/html");
+
+        PrintWriter printWriter = resp.getWriter();
+        try {
+            printWriter.println("<h2>Time: = " + LocalDate.now() + "</h2");
+
+        }
+        finally {
+            printWriter.close();
+        }
+
+
+
+    }
+}
